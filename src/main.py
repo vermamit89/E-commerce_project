67d526a28377bf5a23f9142b30861550bef94659
Amit Verma <vermamit89@gmail.com>
2022-01-02 21:13:38 +0530

    fix:adding database to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from .routes import common
+from .models import database
 
 app=FastAPI()
+
+database.base.metadata.create_all(database.engine)
 
 app.include_router(common.router)
