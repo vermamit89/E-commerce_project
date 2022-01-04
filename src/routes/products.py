d1dd8fd745420a698f2cb2814a95ab721ec6464f
Amit Verma <vermamit89@gmail.com>
2022-01-04 23:29:41 +0530

    fix:creating user routes

--- src/routes/products.py
+++ src/routes/products.py
@@ -9,16 +9,16 @@ router=APIRouter(
 
 
 
-@router.get('/', status_code=status.HTTP_200_OK)
-def all(db:Session=Depends(database.get_db)):
-    products=db.query(models.Product).all()
-    return products
+# @router.get('/', status_code=status.HTTP_200_OK)
+# def all(db:Session=Depends(database.get_db)):
+#     products=db.query(models.Product).all()
+#     return products
 
-@router.get('/{id}')
-def show_1(id,db:Session=Depends(database.get_db)):
-    product_1=db.query(models.Product).filter(models.Product.id==id).first()
-    if not product_1:
-        raise HTTPException(status_code=404,detail=f'product with the id {id} not found')
+# @router.get('/{id}')
+# def show_1(id,db:Session=Depends(database.get_db)):
+#     product_1=db.query(models.Product).filter(models.Product.id==id).first()
+#     if not product_1:
+#         raise HTTPException(status_code=404,detail=f'product with the id {id} not found')
 
-    return product_1
+#     return product_1
 
--- src/routes/user.py
+++ src/routes/user.py
@@ -19,3 +19,16 @@ def create_user(req:schemas.User_creation,db: Session=Depends(database.get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.get('/', status_code=status.HTTP_200_OK)
+def all(db:Session=Depends(database.get_db)):
+    products=db.query(models.Product).all()
+    return products
+
+@router.get('/{id}')
+def show_1(id,db:Session=Depends(database.get_db)):
+    product_1=db.query(models.Product).filter(models.Product.id==id).first()
+    if not product_1:
+        raise HTTPException(status_code=404,detail=f'product with the id {id} not found')
+
+    return product_1
