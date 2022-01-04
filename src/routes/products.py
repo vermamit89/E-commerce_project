aedf952ba6dcf9e69f977d0bcf86b209a269e7d7
Amit Verma <vermamit89@gmail.com>
2022-01-04 22:51:57 +0530

    wip:getting  singal product

--- src/routes/products.py
+++ src/routes/products.py
@@ -19,3 +19,11 @@ def add_product(req:schemas.Products,db:Session=Depends(database.get_db)):
 def all(db:Session=Depends(database.get_db)):
     products=db.query(models.Product).all()
     return products
+
+@router.get('/{id}')
+def show_1(id,db:Session=Depends(database.get_db)):
+    product_1=db.query(models.Product).filter(models.Product.id==id).first()
+    if not product_1:
+        raise HTTPException(status_code=404,detail=f'product with the id {id} not found')
+
+    return product_1
